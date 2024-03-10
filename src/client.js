d014b561d26d116490cfdffd7a13c0178c770621
mina <yegrinaaa@gmail.com>
2024-03-10 23:59:36 +0900

    client-server 연동

---
+++ src/client.js
@@ -0,0 +1,21 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      {
+        allMovies {
+          title
+        }
+      }
+    `,
+  })
+  .then((data) => console.log(data));
+
+export default client;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import client from './client';
+import { ApolloProvider } from '@apollo/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
