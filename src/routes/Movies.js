494d55d962165505bf165230d8a080722059dfe8
mina <yegrinaaa@gmail.com>
2024-03-14 02:02:18 +0900

    tweets 데이터도 불러와 보기

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,5 +1,4 @@
 import { useQuery } from '@apollo/client';
-
 import { gql } from 'graphql-tag';
 
 const ALL_MOVIES = gql`
@@ -8,23 +7,38 @@ const ALL_MOVIES = gql`
       id
       title
     }
+    allTweets {
+      id
+      text
+      author {
+        fullName
+      }
+    }
   }
 `;
 
 export default function Movies() {
   const { data, loading, error } = useQuery(ALL_MOVIES);
+  // 다른 데이터도 불러보기
+  // console.log('allTweets', data.allTweets);
 
   if (loading) {
-    return <h1>loading...</h1>;
+    return <h1>Loading...</h1>;
   }
   if (error) {
-    return <h1>could not fetch</h1>;
+    return <h1>Could not fetch 😢</h1>;
   }
 
   return (
     <ul>
+      <h1>Movies</h1>
       {data.allMovies.map((movie) => (
         <li key={movie.id}>{movie.title}</li>
+      ))}
+
+      <h1>Tweets</h1>
+      {data.allTweets.map((tweet) => (
+        <li key={tweet.id}>{`${tweet.text} - ${tweet.author.fullName}`}</li>
       ))}
     </ul>
   );
