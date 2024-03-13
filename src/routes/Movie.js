836024fcea9c67e8ddacb047bc9443d726be016d
mina <yegrinaaa@gmail.com>
2024-03-14 02:55:12 +0900

    local only fields

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -7,24 +7,34 @@ const GET_MOVIE = gql`
     movie(id: $movieId) {
       id
       title
+      small_cover_image
+      rating
+      isLiked @client
     }
   }
 `;
 
 export default function Movie() {
-  // const params = useParams();
   const { id } = useParams();
 
   const { data, loading } = useQuery(GET_MOVIE, {
     variables: {
-      // movieId: params.id,
       movieId: id,
     },
   });
+
+  const onClick = () => {};
 
   if (loading) {
     return <h1>Loading Movie...</h1>;
   }
 
-  return <div>{data.movie.title}</div>;
+  return (
+    <div>
+      {data.movie.title}
+      <img src={data.movie.small_cover_image} alt="커버 이미지" />
+      <div>rating: {data.movie.rating}</div>
+      <button onClick={onClick}>{data.movie.isLike ? 'UnLike' : 'Like'}</button>
+    </div>
+  );
 }
