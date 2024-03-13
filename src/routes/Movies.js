4a18db0a0ba00c8eef3d521dc72d88cde45d406a
mina <yegrinaaa@gmail.com>
2024-03-14 01:41:51 +0900

    영화 리스트 graphQL 로 연동해서 화면에 랜더링

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,3 +1,32 @@
+import { useApolloClient } from '@apollo/client';
+import { useEffect, useState } from 'react';
+import { gql } from 'graphql-tag';
+
 export default function Movies() {
-  return <div>this is list of movie</div>;
+  const client = useApolloClient();
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          {
+            allMovies {
+              id
+              title
+            }
+          }
+        `,
+      })
+      .then((result) => setMovies(result.data.allMovies));
+  }, [client]);
+
+  console.log(movies);
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </div>
+  );
 }
